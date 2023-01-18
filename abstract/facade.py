8d28584215714c4f40cc0ab4f2d89b21f60fcf12
Ilya <nestyreff@ya.ru>
2023-01-18 10:44:19 +0300

    abstract update
    F renamed abc->abstract
    + facade mark class

---
+++ abstract/facade.py
@@ -0,0 +1,7 @@
+""" This file contains Facade abstract class """
+
+
+class Facade:
+    """ This class is used to mark and in the future add functionality to all
+    classses which will implement facade pattern """
+    pass
