537f2014634e121e7cc103160fed385bf98da2c4
Ilya <nestyreff@ya.ru>
2023-01-18 14:31:24 +0300

    Add comments

--- bot/middlewares/translator_md.py
+++ bot/middlewares/translator_md.py
@@ -19,9 +19,15 @@ class TranslatorMiddleware(BaseMiddleware):
     ) -> Any:
         translator: Translator = data['translator']
         if conf.translate.locale_identify_mode == LocaleIdentificationMode.BY_TELEGRAM_LOCALE:
+            """ Get locale from user language code """
             data['translator'] = translator(language=event.from_user.language_code)
         elif conf.translate.locale_identify_mode == LocaleIdentificationMode.BY_DATABASE:
+            """ Get locale from cache """
             cache: Cache = data['cache']
-            data['translator'] = translator(language=await cache.get(LocaleScheme(user_id=event.from_user.id)))
-
-
+            locale_key = LocaleScheme(user_id=event.from_user.id)
+            # Use default locale
+            locale = conf.translate.default_locale
+            if await cache.exists(locale_key):
+                # If any locale key were set then use it
+                locale = await cache.get()
+            data['translator'] = translator(language=locale)
