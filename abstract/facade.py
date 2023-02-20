893d484eed4efd575ac2c0845e293623725560dd
Ilya <nestyreff@ya.ru>
2023-02-21 01:45:51 +0300

    some deletions

--- abstract/facade.py
+++
@@ -1,7 +0,0 @@
-""" This file contains Facade abstract class """
-
-
-class Facade:
-    """ This class is used to mark and in the future add functionality to all
-    classses which will implement facade pattern """
-    pass
