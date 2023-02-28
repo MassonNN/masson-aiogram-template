8cef14af50597871c5990560cb2656c96607f436
Ilya <nestyreff@ya.ru>
2023-02-28 15:44:50 +0300

    add fixtures bot, storage and dp

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,9 +1,13 @@
 from typing import Callable
 
+import pytest
 import pytest_asyncio
+from aiogram.fsm.storage.memory import MemoryStorage
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.bot.dispatcher import get_dispatcher
 from src.db.database import create_session_maker
+from tests.utils.mocked_bot import MockedBot
 from tests.utils.mocked_database import MockedDatabase
 
 
@@ -21,3 +25,18 @@ async def db(pool: Callable[[], AsyncSession]):
     yield database
     await database.teardown()
     await session.close()
+
+
+@pytest.fixture()
+def bot():
+    return MockedBot()
+
+
+@pytest.fixture()
+def storage():
+    return MemoryStorage()
+
+
+@pytest.fixture()
+def dp(storage):
+    return get_dispatcher(storage=storage)
