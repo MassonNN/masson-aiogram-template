1d29fe910d0e9b4ae2fbc9282b1cfb010f38d7ae
Ilya <nestyreff@ya.ru>
2023-08-01 14:44:34 +0300

    Fix redis client creation

--- src/bot/__main__.py
+++ src/bot/__main__.py
@@ -3,28 +3,35 @@ import asyncio
 import logging
 
 from aiogram import Bot
+from redis.asyncio.client import Redis
 
 from src.bot.dispatcher import get_dispatcher, get_redis_storage
 from src.bot.structures.data_structure import TransferData
-from src.cache import Cache
+from src.db.database import create_async_engine
 from src.configuration import conf
-from src.db.database import create_session_maker
-from src.language import Translator
 
 
 async def start_bot():
     """This function will start bot with polling mode."""
     bot = Bot(token=conf.bot.token)
-    cache = Cache()
-    storage = get_redis_storage(redis=cache.redis_client)
+    storage = get_redis_storage(
+        redis=Redis(
+            db=conf.redis.db,
+            host=conf.redis.host,
+            password=conf.redis.passwd,
+            username=conf.redis.username,
+            port=conf.redis.port
+        )
+    )
     dp = get_dispatcher(storage=storage)
 
     await dp.start_polling(
         bot,
         allowed_updates=dp.resolve_used_update_types(),
         **TransferData(
-            pool=create_session_maker(), translator=Translator(), cache=cache
+            engine=create_async_engine(url=conf.db.build_connection_str())
         )
+
     )
 
 
