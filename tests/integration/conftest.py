d38bbc9cca1a9bac58c022bcae616da5b7895d8d
Ilya <nestyreff@ya.ru>
2023-08-01 14:47:17 +0300

    update conftest.py

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,32 +1,30 @@
 """Configuration for integrational tests."""
-from collections.abc import Callable
-
 import pytest
 import pytest_asyncio
 from aiogram.fsm.storage.memory import MemoryStorage
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import AsyncSession, AsyncEngine
 
 from src.bot.dispatcher import get_dispatcher
-from src.db.database import create_session_maker
 from tests.utils.mocked_bot import MockedBot
 from tests.utils.mocked_database import MockedDatabase
 
 
-@pytest_asyncio.fixture()
-async def pool():
-    """Pool (sessionmaker) fixture."""
-    pool = create_session_maker()
-    yield pool
-    await pool.close_all()  # noqa
+@pytest_asyncio.fixture(scope="function")
+async def session(engine: AsyncEngine) -> AsyncSession:
+    """Async session fixture.
+
+    :param engine: Bind engine for open session
+    """
+    async with AsyncSession(bind=engine) as session:
+        yield session
 
 
-@pytest_asyncio.fixture()
-async def db(pool: Callable[[], AsyncSession]):
+@pytest_asyncio.fixture(scope="function")
+async def db(session: AsyncSession):
     """Database fixture."""
-    async with pool() as session:  # type: AsyncSession
-        database = MockedDatabase(session)
-        yield database
-        await database.teardown()
+    database = MockedDatabase(session)
+    yield database
+    await database.teardown()
 
 
 @pytest.fixture()
