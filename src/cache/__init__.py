b6a56655a96ef191fbb8f86fa85c49d0b8e4b127
Ilya <nestyreff@ya.ru>
2023-08-01 14:16:45 +0300

    Remove useless cache adapter

--- src/cache/__init__.py
+++
@@ -1,4 +0,0 @@
-"""This package is used for cache adapter."""
-from cache.adapter import Cache
-
-__all__ = ('Cache',)
--- src/language/__init__.py
+++
@@ -1,2 +0,0 @@
-"""This package is used for internationalization tools."""
-from .translator import Translator, LocalizedTranslator, LocaleScheme
